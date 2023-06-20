ff7bd709fbf6aa0d13feaa20b87d50c1a4f16307
Bassie265 <bastiaansnoeij@gmail.com>
2023-06-20 12:59:09 +0200

    afmaken hoe rijden

--- hoofdprogramma/AGV_Hoofdprogramma/Hoe_rijden.c
+++ hoofdprogramma/AGV_Hoofdprogramma/Hoe_rijden.c
@@ -2,40 +2,56 @@
 #define afstandmuur 50
 #define margemeer 55
 #define margeminder 45
-#define afstandmuurmeer 60
-#define afstandmuurminder 40
 
 int rijden(int tv, int ta)    //tv = TOFvoorwaarde
 {                             //ta = TOFachterwaarde
     if(tv > margemeer && ta > margemeer){
         //motor langzaam rechts
-        DirectieMotorL = 0
-        DirectieMotorR = 1
-
+        if (MotorTrackingPositieL <4){
+        MotorTrackingPositieL++;
+        DirectieMotorL = 1;
+        DirectieMotorR = 0;
+        }
+        else {
+            DirectieMotorL = 1;
+            DirectieMotorR = 1;
+        }
+    }
+    if (!(tv > margemeer && ta > margemeer)){
+        MotorTrackingPositieL = 0;
     }
     else if(tv < margeminder, ta < margeminder){
         //motor langzaam links
+        if (MotorTrackingPositieR <4){
+        MotorTrackingPositieR++;
+        DirectieMotorL = 0;
+        DirectieMotorR = 1;
+        }
+        else {
+            DirectieMotorL = 1;
+            DirectieMotorR = 1;
+        }
     }
-
-
-
+    if (!(tv < margeminder, ta < margeminder)){
+        MotorTrackingPositieR = 0;
+    }
     else if(tv == ta){
-        //motor
+        //rechtdoorrijden
+        DirectieMotorL = 1;
+        DirectieMotorR = 1;
     }
     else if(tv > ta){
-        if(tv >= afstandmuurmeer){
-            //motorheel veel rechts
-        }
-        else{
-            //motor klein beetje rechts
+        if(tv >= afstandmuur){
+            //motorheel rechts
+            DirectieMotorL = 1;
+            DirectieMotorR = 0;
         }
     }
     else if(tv < ta){
-        if(tv <= afstandmuurminder){
+        if(tv < afstandmuur){
             //motor veel links
-        }
-        else {
-            //motor beetje links
+            DirectieMotorL = 0;
+            DirectieMotorR = 1;
         }
     }
 }
