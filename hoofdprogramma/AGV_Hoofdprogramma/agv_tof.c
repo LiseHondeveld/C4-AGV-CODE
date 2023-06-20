39aebc03f97d6d0c55158660c0ba2d68dbeab951
Esmee513 <EsmeevdSter+github@gmail.com>
2023-06-20 14:58:49 +0200

    tof bestanden toegevoegd

---
+++ hoofdprogramma/AGV_Hoofdprogramma/agv_tof.c
@@ -0,0 +1,16 @@
+#include "agv_tof.h"
+
+/*uint8_t VL53L0X_TestRead(uint8_t addr)
+{
+    uint8_t device_id = 0;
+    if (!i2c_read_addr8_data8(REG_IDENTIFICATION_MODEL_ID, &device_id)) {
+        return false;
+    }
+    return device_id == VL53L0X_EXPECTED_DEVICE_ID;
+}
+
+void VL53L0X_DataInit(uint8_t addr)
+{
+
+}
+*/
