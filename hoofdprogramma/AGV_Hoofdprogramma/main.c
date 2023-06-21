a3bd7a504717c40ae7142946e687cdc85b1e42e9
LiseHondeveld <134971030+LiseHondeveld@users.noreply.github.com>
2023-06-22 00:03:34 +0200

    main aangepast

--- hoofdprogramma/AGV_Hoofdprogramma/main.c
+++ hoofdprogramma/AGV_Hoofdprogramma/main.c
@@ -13,18 +13,13 @@ int main(void)
 {
    agv_buzzer_init();
    agv_ultrasoon_init();
-
-   //agv_buzzer_aan();
-   //_delay_ms(1000);
-   //agv_buzzer_uit();
-   //_delay_ms(1000);
    agv_motor_init();
    PredefinedBocht();
-   //DirectieMotorL = -1;
-   //DirectieMotorR = 1;
+   agv_motor_init();
+
    while(1){
 
-    //boomdetectie();
+    boomdetectie();
     obstakeldetectie();
 
    }
