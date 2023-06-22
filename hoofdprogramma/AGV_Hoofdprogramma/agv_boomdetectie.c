0f59647e44a8559b49b2450320ecc570cb52dd43
Sander2511 <132059730+Sander2511@users.noreply.github.com>
2023-06-22 10:05:09 +0200

    Timer voor boomdetectie

--- hoofdprogramma/AGV_Hoofdprogramma/agv_boomdetectie.c
+++ hoofdprogramma/AGV_Hoofdprogramma/agv_boomdetectie.c
@@ -7,30 +7,44 @@ void boomdetectie(void)
 {
     if (agv_ultrasoon_boom_links <= 10)
     {
-        DirectieMotorL = 0;
-        DirectieMotorR = 0;// karretje niet rijden
+        static unsigned long long int tijdlaatsteboomlinks = 0;
 
-        agv_buzzer_aan();
-        _delay_ms(400);
-        agv_buzzer_uit();
-        _delay_ms(1000);
-        // buzzer aan voor 1 sec
+        if (time_current_ms(0)- tijdlaatsteboomlinks > 1750)
+        {
+            DirectieMotorL = 0;
+            DirectieMotorR = 0;// karretje niet rijden
+            agv_buzzer_aan();
+            _delay_ms(400);
+            agv_buzzer_uit();
+            _delay_ms(1000);
+            tijdlaatsteboomlinks = time_current_ms(0);
+            // buzzer aan voor 1 sec
+        }
+
     }
     if (agv_ultrasoon_boom_rechts <=10)
     {
-        DirectieMotorL = 0;
-        DirectieMotorR = 0;// karretje niet rijden
+        static unsigned long long int tijdlaatsteboomrechts = 0;
 
-        agv_buzzer_aan(); // buzzer aan voor 1 sec
-        _delay_ms(400);
-        agv_buzzer_uit();
-        _delay_ms(1000);
+        if(time_current_ms(0) - tijdlaatsteboomrechts > 1750)
+        {
+
+            DirectieMotorL = 0;
+            DirectieMotorR = 0;// karretje niet rijden
+
+            agv_buzzer_aan(); // buzzer aan voor 1 sec
+            _delay_ms(400);
+            agv_buzzer_uit();
+            _delay_ms(1000);
+            tijdlaatsteboomrechts = time_current_ms(0);
+        }
+
     }
 }
 
 void obstakeldetectie(void)
 {
-    if ((agv_ultrasoon_voor_midden <= 7) || (agv_ultrasoon_voor_rechts <= 7) || (agv_ultrasoon_voor_links <= 7))
+    if ((agv_ultrasoon_voor_midden <= 7))
     {
         TIMSK4 &= ~(1<<TOIE4);
         agv_buzzer_aan();
--- hoofdprogramma/AGV_Hoofdprogramma/main.c
+++ hoofdprogramma/AGV_Hoofdprogramma/main.c
@@ -14,12 +14,15 @@ int main(void)
    agv_buzzer_init();
    agv_ultrasoon_init();
    agv_motor_init();
-   PredefinedBocht();
-   agv_motor_init();
+   DirectieMotorL = 1;
+   DirectieMotorR = 1;
+   //PredefinedBocht();
 
    while(1){
 
     boomdetectie();
+    DirectieMotorL = 1;
+    DirectieMotorR = 1;
     obstakeldetectie();
 
    }
