c90f3f8f266e8c4db7d862be72e63da4dae11b86
Sander2511 <132059730+Sander2511@users.noreply.github.com>
2023-06-22 08:44:25 +0200

    Update agv_buzzer.c
    . erachter gezet

--- hoofdprogramma/AGV_Hoofdprogramma/agv_buzzer.c
+++ hoofdprogramma/AGV_Hoofdprogramma/agv_buzzer.c
@@ -5,7 +5,7 @@ void agv_buzzer_init(void) //Functie voor de buzzer
     DDRL |= (1<<PL3); //Pin 46 configureren van de arduino
     TCCR5A = (0<<WGM51) | (1<<WGM50) | (0<<COM5A1); //Instellen fast PWM 8-bit
     TCCR5B |= (1<<CS52) | (0<<CS51) | (0<<CS50); //Prescaler 256
-    TCCR5B |= (0<<WGM53) | (1<<WGM32); //Instellen fast PWM 8-bit
+    TCCR5B |= (0<<WGM53) | (1<<WGM32); //Instellen fast PWM 8-bit.
     OCR5A = 192; //Duty cycle (hier kan je de volume mee aanpassen)
 }
 
